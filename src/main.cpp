63a8ed9db0679124b659636248d4f28fa8f9527a
Josef Adamcik <josef.adamcik@gmail.com>
2021-03-07 13:52:12 +0100

    Connect to wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <Arduino.h>
-
-#include "config.h"
-#include "keys.h"
+#include <BH1750.h>
 #include <WEMOS_SHT3X.h>
 #include <Wire.h>
-#include <BH1750.h>
+#include <mqtt_client.h>
+
+#include "WiFi.h"
+#include "config.h"
+#include "keys.h"
+
+#define LED_RED_PIN 33
+#define LED_GREEN_PIN 32
 
 BH1750 lightSensor;
 SHT3X sht30(0x45);
@@ -35,25 +40,74 @@ SHT3X sht30(0x45);
 //     ArduinoOTA.begin();
 // }
 
-void setup() { 
-    Serial.begin(115200); 
+
+void ledOkState(bool on) {
+    digitalWrite(LED_GREEN_PIN, on);
+}
+
+void ledErrState(bool on) {
+    digitalWrite(LED_RED_PIN, on);
+}
+
+
+void ledState(bool ok) {
+    ledOkState(ok);
+    ledErrState(!ok);
+}
+
+void connectToWifi() {
+    if (WiFi.status() != WL_CONNECTED) {
+        ledOkState(false);
+        ledErrState(false);
+        WiFi.begin(MYSSID, MYPASS);
+        while (WiFi.status() != WL_CONNECTED) {
+            Serial.println("Connecting to WiFi..");
+            delay(250);
+            ledOkState(true);
+            delay(250);
+            ledOkState(false);
+
+            if (WiFi.status() == WL_CONNECT_FAILED) {
+                Serial.println("Wifi connection failed");
+                break;
+            }
+        }
+    }
+    if (WiFi.status() == WL_CONNECTED) {
+        ledOkState(true);
+    } else {
+        ledState(false);
+    }
+}
+
+void setup() {
+    Serial.begin(115200);
     Wire.begin();
     lightSensor.begin();
+
+    pinMode(LED_GREEN_PIN, OUTPUT);
+    pinMode(LED_RED_PIN, OUTPUT);
+    ledState(true);
 }
 
 void loop() {
+    connectToWifi();
     if (sht30.get() == 0) {
         Serial.print("Temperature in Celsius : ");
         Serial.println(sht30.cTemp);
         Serial.print("Relative Humidity : ");
         Serial.println(sht30.humidity);
-        Serial.println();
     } else {
+        ledState(false);
         Serial.println("Error!");
     }
+
     float lux = lightSensor.readLightLevel();
     Serial.print("Light: ");
     Serial.print(lux);
-    Serial.println(" lx");
+    Serial.println("lx");
+
+    Serial.println();
+
     delay(1000);
 }
