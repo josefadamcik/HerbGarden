f8b153e6dd8b7eb85a6813e051bd07db132312df
Josef Adamcik <josef.adamcik@gmail.com>
2021-03-07 14:47:07 +0100

    Add OTA

--- src/config.h
+++ src/config.h
@@ -1,1 +1,7 @@
 #define HOSTNAME "herbgarden.local"
+#define MQTT_BROKER "http://192.168.178.58:1883"
+#define MQTT_TOPI_PREFIX = "home/herbgarden/"
+
+IPAddress ip(192, 168, 178, 69);
+IPAddress gateway(192, 168, 178, 1);
+IPAddress subnet(255, 255, 255, 0);
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <ArduinoOTA.h>
+#include <Wire.h>
 #include <BH1750.h>
 #include <WEMOS_SHT3X.h>
-#include <Wire.h>
 #include <mqtt_client.h>
 
-#include "WiFi.h"
 #include "config.h"
 #include "keys.h"
 
@@ -13,33 +14,6 @@
 
 BH1750 lightSensor;
 SHT3X sht30(0x45);
-
-// void otaInitialize() {
-//     // Initialise OTA in case there is a software upgrade
-//     ArduinoOTA.setHostname(HOSTNAME);
-//     ArduinoOTA.onStart([]() { Serial.println("Start"); });
-//     ArduinoOTA.onEnd([]() { Serial.println("\nEnd"); });
-//     ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-//         Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-//     });
-//     ArduinoOTA.onError([](ota_error_t error) {
-//         Serial.printf("Error[%u]: ", error);
-//         if (error == OTA_AUTH_ERROR) {
-//             Serial.println("Auth Failed");
-//         } else if (error == OTA_BEGIN_ERROR) {
-//             Serial.println("Begin Failed");
-//         } else if (error == OTA_CONNECT_ERROR) {
-//             Serial.println("Connect Failed");
-//         } else if (error == OTA_RECEIVE_ERROR) {
-//             Serial.println("Receive Failed");
-//         } else if (error == OTA_END_ERROR) {
-//             Serial.println("End Failed");
-//         }
-//     });
-
-//     ArduinoOTA.begin();
-// }
-
 
 void ledOkState(bool on) {
     digitalWrite(LED_GREEN_PIN, on);
@@ -55,10 +29,22 @@ void ledState(bool ok) {
     ledErrState(!ok);
 }
 
+void onWifiConnected() {
+    Serial.println("Wifi connected.");
+    Serial.print("IP: ");
+    Serial.print(WiFi.localIP());
+    Serial.println();
+    ArduinoOTA.begin();
+}
+
 void connectToWifi() {
+    bool connectionAttempt = false;
     if (WiFi.status() != WL_CONNECTED) {
+        connectionAttempt = true;
         ledOkState(false);
         ledErrState(false);
+        WiFi.config(ip, gateway, subnet, gateway, gateway);
+        WiFi.setHostname(HOSTNAME);
         WiFi.begin(MYSSID, MYPASS);
         while (WiFi.status() != WL_CONNECTED) {
             Serial.println("Connecting to WiFi..");
@@ -75,9 +61,53 @@ void connectToWifi() {
     }
     if (WiFi.status() == WL_CONNECTED) {
         ledOkState(true);
+        if (connectionAttempt) {
+            onWifiConnected();
+        }
     } else {
         ledState(false);
     }
+}
+
+void setupOta() {
+     // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  // Hostname defaults to esp3232-[MAC]
+  ArduinoOTA.setHostname(HOSTNAME);
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
 }
 
 void setup() {
@@ -88,10 +118,10 @@ void setup() {
     pinMode(LED_GREEN_PIN, OUTPUT);
     pinMode(LED_RED_PIN, OUTPUT);
     ledState(true);
+    setupOta();
 }
 
-void loop() {
-    connectToWifi();
+void measure() {
     if (sht30.get() == 0) {
         Serial.print("Temperature in Celsius : ");
         Serial.println(sht30.cTemp);
@@ -108,6 +138,11 @@ void loop() {
     Serial.println("lx");
 
     Serial.println();
+}
 
+void loop() {
+    connectToWifi();
+    ArduinoOTA.handle();
+    measure();
     delay(1000);
 }
